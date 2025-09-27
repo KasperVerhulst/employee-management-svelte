113f1b8a20e8338ccdd6324df37c040382ce1b6a
Kasper Verhulst <kasper.verhulst@devoteam.com>
2025-09-27 10:19:59 +0200

    feat: introduce Ping JavaScript SDK
    Connect to Ping Identity Server to obtain an OAuth2 access token that will be included in API requests

---
+++ src/lib/oauth.ts
@@ -0,0 +1,61 @@
+import {
+	PUBLIC_SCOPE,
+	PUBLIC_WEB_OAUTH_CLIENT,
+	PUBLIC_WELL_KNOWN,
+	PUBLIC_REDIRECT_URI
+} from '$env/static/public';
+import * as forgerock from '@forgerock/javascript-sdk';
+
+let initializeSDK: Promise<void> | null = null;
+
+/**
+ * Initializes the ForgeRock SDK configuration.
+ */
+function initialize(): Promise<void> {
+	if (!initializeSDK) {
+		initializeSDK = forgerock.Config.setAsync({
+			clientId: PUBLIC_WEB_OAUTH_CLIENT,
+			redirectUri: PUBLIC_REDIRECT_URI,
+			scope: PUBLIC_SCOPE,
+			serverConfig: {
+				wellknown: PUBLIC_WELL_KNOWN
+			}
+		});
+		console.debug('ForgeRock SDK configured successfully.');
+	} else {
+		console.debug('ForgeRock SDK is already initialized.');
+	}
+	return initializeSDK;
+}
+
+export async function oauth2flow(): Promise<forgerock.OAuth2Tokens | void> {
+	await initialize(); //ensure SDK is initialized
+	const tokens = await forgerock.TokenManager.getTokens({
+		login: 'redirect', // Redirect to the server or the web app that handles authentication
+		forceRenew: false, // Immediately return stored tokens, if they exist
+		skipBackgroundRequest: true, // Regardless of session status, redirect to the authorization server to initiate the OAuth 2.0 flow
+		query: {
+			acr_values: 'PersonaLogin'
+		}
+	});
+	console.log('Tokens:', tokens);
+	return tokens;
+}
+
+export async function exchangeTokens(
+	code: string,
+	state: string
+): Promise<forgerock.OAuth2Tokens | void> {
+	await initialize(); //ensure SDK is initialized
+
+	// Exchange the authorization code for tokens
+	const tokens = await forgerock.TokenManager.getTokens({
+		query: {
+			code,
+			state
+		}
+	});
+
+	console.log('Tokens received:', tokens);
+	return tokens;
+}
